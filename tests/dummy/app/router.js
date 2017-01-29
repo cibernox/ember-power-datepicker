2ea1bfc2397fdbfbb5ad93bc6f4b1982ac8704f4
cibernox <miguel.camba@gmail.com>
2017-01-29 15:22:36 +0000

    Save progress

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -7,6 +7,10 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('public-pages', { path: '' }, function() {
+
+  });
+  this.route('legacy-demo');
 });
 
 export default Router;
