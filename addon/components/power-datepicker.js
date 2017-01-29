e6f3112b1e6ebf582f7ab6bc61ad1e0801e6917b
cibernox <miguel.camba@gmail.com>
2017-01-29 14:58:30 +0000

    Add default behaviour for `onCenterChange`

--- addon/components/power-datepicker.js
+++ addon/components/power-datepicker.js
@@ -1,11 +1,27 @@
 import Ember from 'ember';
 import layout from '../templates/components/power-datepicker';
 import service from 'ember-service/inject';
+import moment from 'moment';
+import computed from 'ember-computed';
+
+function fallbackAction(fallback) {
+  return computed({
+    get() {
+      return fallback.bind(this);
+    },
+    set(_, v) {
+      return v === undefined ? fallback.bind(this) : v;
+    }
+  });
+}
 
 export default Ember.Component.extend({
   layout,
   moment: service(),
   format: 'YYYY/MM/DD',
+  onCenterChange: fallbackAction(function(day){
+    this.set('center', day.date);
+  }),
 
   // Actions
   actions: {
