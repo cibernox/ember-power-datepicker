a20d62bebc5432341813740d71bb709ba195bb91
cibernox <miguel.camba@gmail.com>
2017-01-29 12:44:27 +0000

    Update to latest ember-cli & ember

--- addon/components/power-datepicker.js
+++ addon/components/power-datepicker.js
@@ -1,6 +1,36 @@
 import Ember from 'ember';
 import layout from '../templates/components/power-datepicker';
+import service from 'ember-service/inject';
 
 export default Ember.Component.extend({
-  layout
+  layout,
+  moment: service(),
+  format: 'YYYY/MM/DD',
+
+  // Actions
+  actions: {
+    handleInput(dropdown, calendar, e) {
+      let format =  this.get('format');
+      let maybeDate = e.target.value;
+      if (format.length > maybeDate.length) {
+        return;
+      }
+      let parsedMoment = this.get('moment').moment(maybeDate, format);
+      if (parsedMoment.isValid()) {
+        calendar.actions.select({ moment: parsedMoment, date: parsedMoment.toDate() });
+      }
+    },
+
+    handleSelect(dropdown, day, e) {
+      this.get('onSelect')(day, dropdown, e);
+      dropdown.actions.close();
+    }
+  },
+
+  // Helpers
+  formatDate(selected, format) {
+    if (selected) {
+      return moment(selected).format(format);
+    }
+  }
 });
