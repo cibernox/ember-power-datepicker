42b18484fd2fd1c475bf5f4e83ba21745e44bcc9
cibernox <miguel.camba@gmail.com>
2017-03-03 16:13:52 +0000

    Add the basic-dropdown destination

--- index.js
+++ index.js
@@ -2,5 +2,10 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-power-datepicker'
+  name: 'ember-power-datepicker',
+
+  contentFor(type, config) {
+    let emberBasicDropdown = this.addons.find((a) => a.name === 'ember-basic-dropdown');
+    return emberBasicDropdown.contentFor(type, config);
+  }
 };
