a95c21f6b0df5d9ad2ad614aa5d061f71eac21f1
Manjunath, Vishwaas <Vishwaas_Manjunath@gallup.com>
2017-05-10 20:46:01 -0500

    Adding instance locale support

--- tests/dummy/app/controllers/public-pages/index.js
+++ tests/dummy/app/controllers/public-pages/index.js
@@ -2,12 +2,20 @@ import Ember from 'ember';
 import moment from 'moment';
 const { computed } = Ember;
 export default Ember.Controller.extend({
+  moment: Ember.inject.service(),
   twoDaysAgo: computed(function() {
     return moment().add(-2, 'days');
   }),
   twoWeeksFromNow: computed(function() {
     return moment().add(2, 'weeks');
   }),
+  init() {
+    this._super();
+
+    // loading a particular locale seems to set it as default across the app, so
+    // overriding english as default.
+    this.get('moment').setLocale('en');
+  },
   actions: {
     log() {
       // console.debug(...arguments);
