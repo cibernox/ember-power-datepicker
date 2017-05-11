b2cbb278dbfc76309d788f842b8b75bc309a5b4c
Vish <vishwaas_m@yahoo.co.in>
2017-05-11 14:04:56 -0500

    [Issue-15]Adding instance locale support (#16)
    * Adding instance locale support
    
    * Review change: Loading all fonts
    
    * Minor change

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -6,6 +6,9 @@ module.exports = function(environment) {
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
+    moment: {
+      includeLocales: true
+    },
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
