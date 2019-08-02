5c542922ad1c6a454b0761fad7aa9ecc849cf14a
Miguel Camba <miguel.camba@gmail.com>
2019-08-02 20:35:09 +0200

    Transform classes to ES6 syntax

--- addon/components/power-datepicker.js
+++ addon/components/power-datepicker.js
@@ -1,25 +1,24 @@
 import Component from '@ember/component';
+import { action } from '@ember/object';
 import layout from '../templates/components/power-datepicker';
 import { fallbackAction } from '../utils/computed-properties';
+import { templateLayout, tagName } from "@ember-decorators/component";
 
-export default Component.extend({
-  layout,
-  tagName: '',
-  closeOnSelect: true,
-  format: 'YYYY/MM/DD',
-  onCenterChange: fallbackAction(function(day){
+export default @layout(templateLayout) @tagName('') class extends Component {
+  closeOnSelect = true
+  format = 'YYYY/MM/DD'
+  @fallbackAction(function(day){
     this.set('center', day.date);
-  }),
+  }) onCenterChange
 
   // Actions
-  actions: {
-    onSelect(day, datepicker, e) {
-      let { onSelect, closeOnSelect } = this.getProperties('onSelect', 'closeOnSelect');
-      let value = onSelect(day, datepicker, e);
-      if (value === false || !closeOnSelect) {
-        return;
-      }
-      datepicker.actions.close();
+  @action
+  onSelect(day, datepicker, e) {
+    let { onSelect, closeOnSelect } = this.getProperties('onSelect', 'closeOnSelect');
+    let value = onSelect(day, datepicker, e);
+    if (value === false || !closeOnSelect) {
+      return;
     }
+    datepicker.actions.close();
   }
-});
+}
