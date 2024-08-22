bc5e9cfa9da4ea64f5e8560df7169887c76da2a5
Markus Sanin <markus.sanin@zeppelin-group.com>
2024-08-22 08:34:16 +0200

    Update addon blueprint v2.0.0...v2.17.0

--- ember-power-datepicker/rollup.config.mjs
+++ ember-power-datepicker/rollup.config.mjs
@@ -19,12 +19,17 @@ export default {
     // up your addon's public API. Also make sure your package.json#exports
     // is aligned to the config here.
     // See https://github.com/embroider-build/embroider/blob/main/docs/v2-faq.md#how-can-i-define-the-public-exports-of-my-addon
-    addon.publicEntrypoints(['index.js', 'components/**/*.js']),
+    addon.publicEntrypoints(['index.js', 'components/**/*.js', 'template-registry.js']),
 
     // These are the modules that should get reexported into the traditional
     // "app" tree. Things in here should also be in publicEntrypoints above, but
     // not everything in publicEntrypoints necessarily needs to go here.
-    addon.appReexports(['components/**/*.js']),
+    addon.appReexports([
+      'components/**/*.js',
+      'helpers/**/*.js',
+      'modifiers/**/*.js',
+      'services/**/*.js',
+    ]),
 
     // Follow the V2 Addon rules about dependencies. Your code can import from
     // `dependencies` and `peerDependencies` as well as standard Ember-provided
