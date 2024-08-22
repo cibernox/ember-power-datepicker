55062b3f9255fb0c87237f53b49b4316775e79c1
Markus Sanin <markus.sanin@zeppelin-group.com>
2024-08-22 09:44:24 +0200

    Fix lint

--- ember-power-datepicker/src/types/global.d.ts
+++ ember-power-datepicker/src/types/global.d.ts
@@ -8,11 +8,13 @@ export interface AssignRegistry {
 }
 
 declare module '@glint/environment-ember-loose/registry' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   export default interface Registry
     extends EmbroiderUtilRegistry /* other registries here */ {
     // ...
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   export default interface Registry
     extends AssignRegistry /* other registries here */ {
     // ...
